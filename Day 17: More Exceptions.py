a78f1256e21bd02e66852235a0673e5b76795fe3
Dhyani Desai <65896239+dhyani21@users.noreply.github.com>
2021-05-02 13:17:35 +0530

    Update Day 17: More Exceptions.py

--- Day 17: More Exceptions.py
+++ Day 17: More Exceptions.py
@@ -13,3 +13,14 @@ class Calculator:
             raise Exception("n and p should be non-negative")
         else:
             return pow(n,p)
+        
+        
+myCalculator=Calculator()
+T=int(input())
+for i in range(T):
+    n,p = map(int, input().split())
+    try:
+        ans=myCalculator.power(n,p)
+        print(ans)
+    except Exception as e:
+        print(e)
